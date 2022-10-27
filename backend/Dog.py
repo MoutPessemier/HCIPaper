5f4020e9f30953a1795b99671a385b2b6cd32840
Nam <namle@student.kuleuven.be>
2022-10-27 19:58:36 +0200

    Dog getters toegevoegd en Recommender system al een beetje uitgewerkt. Met name de populatie van de dateset.

--- backend/Dog.py
+++ backend/Dog.py
@@ -1,12 +1,19 @@
 class Dog:
 
-    def __init__(self, name, serial, size, sex, age, dogfriendly):
+    def __init__(self, id, name, size, sex, age, dogfriendly, catfriendly, childfriendly, gardenreq, hug, training, BCD):
+        self.__id = id
         self.__name = name
-        self.__serial = serial
         self.__size = size
         self.__sex = sex
         self.__age = age
         self.__dogfriendly = dogfriendly
+        self.__catfriendly = catfriendly
+        self.__childfriendly = childfriendly
+        self.__gardenreq = gardenreq
+        self.__hug = hug
+        self.__training = training
+        self.__BCD = BCD
+
 
 
     def ToJson(self):
