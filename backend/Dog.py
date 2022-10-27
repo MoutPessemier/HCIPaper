a5e71c9672485c8fcd087e4698ef810895ebb942
Nam <namle@student.kuleuven.be>
2022-10-27 15:39:29 +0200

    added ToJson

--- backend/Dog.py
+++ backend/Dog.py
@@ -1,11 +1,17 @@
 class Dog:
 
-    def __init__(self, naam, volgnummer, grootte, geslacht, leeftijd, hondvriendelijk):
-        self.__naamin = naam
+    def __init__(self, name, volgnummer, grootte, geslacht, leeftijd, hondvriendelijk):
+        self.__name = name
         self.__volgnummerin = volgnummer
         self.__groottein = grootte
         self.__leeftijdin = leeftijd
         self.__hondvriendelijkin = hondvriendelijk
 
-    def geefgegevens(self):
-        return "Het dier noemt" + " " + self.__naamin + " en heeft volgnummer " + str(self.__volgnummerin)
+
+    def ToJson(self):
+        return {
+            name: self.__name,
+
+        }
+
+#output in json
