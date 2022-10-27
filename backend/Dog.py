9f0044c81d8c6a8d116ed8f36823084be74cb9c5
Nam <namle@student.kuleuven.be>
2022-10-27 15:34:30 +0200

    Added class dog

---
+++ backend/Dog.py
@@ -0,0 +1,11 @@
+class Dog:
+
+    def __init__(self, naam, volgnummer, grootte, geslacht, leeftijd, hondvriendelijk):
+        self.__naamin = naam
+        self.__volgnummerin = volgnummer
+        self.__groottein = grootte
+        self.__leeftijdin = leeftijd
+        self.__hondvriendelijkin = hondvriendelijk
+
+    def geefgegevens(self):
+        return "Het dier noemt" + " " + self.__naamin + " en heeft volgnummer " + str(self.__volgnummerin)
--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,3 @@
-
-
-class Dier:
-
-    def __init__(self, naam, volgnummer, grootte, geslacht, leeftijd, hondvriendelijk)
-        self.__naamin = naam
-        self.__volgnummerin = volgnummer
-        self.__groottein = grootte
-        self.__leeftijdin = leeftijd
-        self.__hondvriendelijkin = hondvriendelijk
-
-
-    def geefgegevens(self):
-
-        return "Het dier noemt" + " " + self.__naamin + " en heeft volgnummer " + str(self.__volgnummerin)
-
-
+if __name__ == '__main__':
+    pass
 
