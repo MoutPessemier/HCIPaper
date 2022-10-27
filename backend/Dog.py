413b1da18acaae8a8915c82a6929bcc7234f4423
Nam <namle@student.kuleuven.be>
2022-10-27 18:56:19 +0200

    attributen naar het engels gezet

--- backend/Dog.py
+++ backend/Dog.py
@@ -1,11 +1,12 @@
 class Dog:
 
-    def __init__(self, name, volgnummer, grootte, geslacht, leeftijd, hondvriendelijk):
+    def __init__(self, name, serial, size, sex, age, dogfriendly):
         self.__name = name
-        self.__volgnummerin = volgnummer
-        self.__groottein = grootte
-        self.__leeftijdin = leeftijd
-        self.__hondvriendelijkin = hondvriendelijk
+        self.__serial = serial
+        self.__size = size
+        self.__sex = sex
+        self.__age = age
+        self.__dogfriendly = dogfriendly
 
 
     def ToJson(self):
