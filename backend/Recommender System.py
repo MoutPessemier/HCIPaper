7b3e9ba13c46f31c5bbf0d97f28037092385b959
Nam <namle@student.kuleuven.be>
2022-10-27 19:58:36 +0200

    Dog getters toegevoegd en Recommender system al een beetje uitgewerkt. Met name de populatie van de dateset.

---
+++ backend/Recommender System.py
@@ -0,0 +1,104 @@
+class Dog:
+
+    def __init__(self, id, name, size, sex, age, dogfriendly=True, catfriendly=True, childfriendly=True, gardenreq=True, hug =True, training=True, BCD=False):
+        ## dogfriendly, catfriendly, childfriendly, gardenreq, hug, training en BCD moeten worden aangepast zoals in volgend voorbeeld indien anders dan de defaultwaarde:
+        ### Billie = Dier(1, 'Billie', 'teef','klein', 2, dogfriendly=  False, training= False)
+        ### defaultwaardes moeten altijd op het einde van de init staan
+        self.__id = id
+        self.__name = name
+        self.__size = size
+        self.__sex = sex
+        self.__age = age
+        self.__dogfriendly = dogfriendly
+        self.__catfriendly = catfriendly
+        self.__childfriendly = childfriendly
+        self.__gardenreq = gardenreq
+        self.__hug = hug
+        self.__training = training
+        self.__BCD = BCD
+
+    def give_id(self):
+        return self.__id
+
+    def give_name(self):
+        return self.__name
+
+    def give_sex(self):
+        return self.__sex
+
+    def give_size(self):
+        return self.__size
+
+    def give_age(self):
+        return self.__age
+
+    def give_dogfriendly(self):
+        return self.__dogfriendly
+
+    def give_catfriendly(self):
+        return self.__catfriendly
+
+    def give_childfriendly(self):
+        return self.__childfriendly
+
+    def give_gardenreq(self):
+        return self.__gardenreq
+
+    def give_hug(self):
+        return self.__hug
+
+    def give_training(self):
+        return self.__training
+
+    def give_BCD(self):
+        return self.__BCD
+
+
+
+class Recommender:
+
+    def __init__(self):
+        self.__recommender = []  # lijst van reservaties maken; handig bij opvragen van gegevens
+        self.__recommender_data = pd.DataFrame(columns=['dog_id', 'name', 'size', 'sex', 'age', 'dogfriendly', 'catfriendly','childfriendly','gardenreq','hug','training','BCD','score'])  # dataframe maken; handig voor doorzoeken
+'''
+    def append_data_recommender(self, recommender):
+        self.__recommender.append(recommender)  # reservatie toevoegen aan reservatielijst en gegevens reservaties toevoegen aan dataframe
+        self.__recommender_data = self.__recommender_data.append(
+            {'dog_id': reservatie.geef_reservatie_nummer(), 'name': reservatie.geef_auto().geef_volgnummer_auto(),
+             'size': reservatie.geef_starttijd(), 'sex': reservatie.geef_eindtijd(),
+             'age': reservatie.geef_beginlocatie(), 'dogfriendly': reservatie.geef_eind_plaats(),
+             'catfriendly': reservatie.geef_afstand(), 'childfriendly': reservatie.geef_afstand(),'gardenreq': reservatie.geef_afstand(),
+             'hug': reservatie.geef_afstand(),'training': reservatie.geef_afstand(),'BCD': reservatie.geef_afstand(),'score': reservatie.geef_afstand(),}, ignore_index=True)
+             
+             nieuwe_hond = Dog(id, name, size, sex, age, dogfriendly, catfriendly, childfriendly, gardenreq, hug, training, BCD)
+        self.__recommender_data = self.__recommender_data.append({'dog_id': nieuwe_hond.give_id(), 'name': nieuwe_hond.give_name(),
+             'size': nieuwe_hond.give_size(), 'sex': nieuwe_hond.give_sex(),
+             'age': nieuwe_hond.give_age(), 'dogfriendly': nieuwe_hond.give_dogfriendly(),
+             'catfriendly': nieuwe_hond.give_catfriendly(), 'childfriendly': nieuwe_hond.give_childfriendly(),'gardenreq': nieuwe_hond.give_gardenreq(),
+             'hug': nieuwe_hond.give_hug(),'training': nieuwe_hond.give_training(),'BCD': nieuwe_hond.give_BCD(),'score': nieuwe_hond.g(),}, ignore_index=True)
+        return nieuwe_hond
+'''
+
+
+def append_dog_data(self, id, name, size, sex, age, dogfriendly, catfriendly, childfriendly, gardenreq, hug, training, BCD, score= 0):
+    try:
+        nieuwe_hond = Dog(id, name, size, sex, age, dogfriendly, catfriendly, childfriendly, gardenreq, hug, training, BCD)
+        self.__recommender_data = self.__recommender_data.append({'dog_id': id, 'name': name, 'size': size, 'sex': sex,
+             'age': age, 'dogfriendly': dogfriendly, 'catfriendly': catfriendly, 'childfriendly': childfriendly,'gardenreq': gardenreq,
+             'hug': hug,'training': training,'BCD': BCD,'score': score,}, ignore_index=True)
+
+        return nieuwe_hond
+
+    except Oepsie as e:
+        print(e)
+
+
+
+
+class Oepsie(Exception):
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return 'Er is een fout gebeurd tijdens het inlezen van de hond'
