3a9324f87eff22db80ebba7ee85d7fe3f1770cbb
Mout Pessemier <moutpessemier@hotmail.com>
2022-10-28 14:38:25 +0200

    file sync

--- frontend/src/js/formType1.js
+++ frontend/src/js/formType1.js
@@ -1,10 +1,10 @@
 const init = () => {
   const form = document.getElementById('form');
   const submitBtn = document.getElementById('submit');
-  submitBtn.addEventListener('submit', e => {
+  submitBtn.addEventListener('click', e => {
     e.preventDefault();
     const endTime = new Date().toString().split(' ')[4];
-    //form.submit();
+    // form.submit();
   });
 };
 
