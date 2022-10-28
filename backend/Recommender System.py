a74e6be50fe404ba0b46175e7051bf914649ef5c
Nam <namle@student.kuleuven.be>
2022-10-28 22:24:52 +0200

    dit is een test

--- backend/Recommender System.py
+++ backend/Recommender System.py
@@ -1,3 +1,11 @@
+#if you are commits behind master: checkout on branch, then pull into current merge from master origin (remote)
+
+import datetime as dt
+import pandas as pd
+import numpy as np
+
+
+
 class Dog:
 
     def __init__(self, id, name, size, sex, age, dogfriendly=True, catfriendly=True, childfriendly=True, gardenreq=True, hug =True, training=True, BCD=False):
@@ -60,6 +68,23 @@ class Recommender:
     def __init__(self):
         self.__recommender = []  # lijst van reservaties maken; handig bij opvragen van gegevens
         self.__recommender_data = pd.DataFrame(columns=['dog_id', 'name', 'size', 'sex', 'age', 'dogfriendly', 'catfriendly','childfriendly','gardenreq','hug','training','BCD','score'])  # dataframe maken; handig voor doorzoeken
+
+    def append_dog_data(self, id, name, size, sex, age, dogfriendly, catfriendly, childfriendly, gardenreq, hug, training, BCD, score= 0):
+        try:
+            nieuwe_hond = Dog(id, name, size, sex, age, dogfriendly, catfriendly, childfriendly, gardenreq, hug, training, BCD)
+            self.__recommender_data = self.__recommender_data.append({'dog_id': id, 'name': name, 'size': size, 'sex': sex,
+                 'age': age, 'dogfriendly': dogfriendly, 'catfriendly': catfriendly, 'childfriendly': childfriendly,'gardenreq': gardenreq,
+                 'hug': hug,'training': training,'BCD': BCD,'score': score,}, ignore_index=True)
+
+            return nieuwe_hond
+
+        except Oepsie as e:
+            print(e)
+
+
+    def give_data(self):
+        return self.__recommender_data
+
 '''
     def append_data_recommender(self, recommender):
         self.__recommender.append(recommender)  # reservatie toevoegen aan reservatielijst en gegevens reservaties toevoegen aan dataframe
@@ -80,20 +105,6 @@ class Recommender:
 '''
 
 
-def append_dog_data(self, id, name, size, sex, age, dogfriendly, catfriendly, childfriendly, gardenreq, hug, training, BCD, score= 0):
-    try:
-        nieuwe_hond = Dog(id, name, size, sex, age, dogfriendly, catfriendly, childfriendly, gardenreq, hug, training, BCD)
-        self.__recommender_data = self.__recommender_data.append({'dog_id': id, 'name': name, 'size': size, 'sex': sex,
-             'age': age, 'dogfriendly': dogfriendly, 'catfriendly': catfriendly, 'childfriendly': childfriendly,'gardenreq': gardenreq,
-             'hug': hug,'training': training,'BCD': BCD,'score': score,}, ignore_index=True)
-
-        return nieuwe_hond
-
-    except Oepsie as e:
-        print(e)
-
-
-
 
 class Oepsie(Exception):
 
@@ -102,3 +113,41 @@ class Oepsie(Exception):
 
     def __str__(self):
         return 'Er is een fout gebeurd tijdens het inlezen van de hond'
+
+class OutputGebruiktAlsInputException(Exception):
+
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return 'Opgelet, de output werd als input gegeven, probeer een ander bestand in te geven!'
+
+# Inlezen data, is enkel leuk voor testing, nog aan te passen later:
+
+bool = False
+
+while not bool:
+
+    print('\n')
+    #value = input("Geef uw bestandsnaam in, gevolgd door enter (ZONDER '.csv' extensie): ")
+
+    try:
+
+        df = pd.read_csv("DogdataTest" + '.csv', nrows=10000)
+        bool = True
+        print('\n')
+        print('Bestand is ok')
+        print('\n')
+
+    except OutputGebruiktAlsInputException as o:
+        print(o)
+
+    except FileNotFoundError:
+
+        print('---------------------------------')
+        print('Opgelet, heeft u toch ".csv" toegevoegd? Dit moet zonder ".csv". Of kijk de bestandsnaam nog eens na.')
+        print('Probeer het opnieuw:')
+        print('---------------------------------')
+
+
+print(df[1,'name'])
