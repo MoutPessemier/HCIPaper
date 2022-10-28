3984d4ffbd890c3e2a23f41c863558c43acea79e
Mout Pessemier <moutpessemier@hotmail.com>
2022-10-28 14:10:47 +0200

    Fixed the dynamic routing

--- frontend/src/js/landingPage.js
+++ frontend/src/js/landingPage.js
@@ -4,7 +4,7 @@ const init = () => {
     const formType = Math.floor(Math.random() * 3) + 1;
     const startTime = new Date().toString().split(' ')[4];
     window.localStorage.setItem('startTime', startTime);
-    window.location.href = `/formType${formType}.html`;
+    window.location.href = `/pages/formType${formType}.html`;
   };
 };
 
