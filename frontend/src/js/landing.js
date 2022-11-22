90623493362944e3f3fc974477dc4cfab480eefc
Mout Pessemier <moutpessemier@hotmail.com>
2022-11-22 14:44:48 +0100

    Finished flow suggestions

--- frontend/src/js/landing.js
+++ frontend/src/js/landing.js
@@ -1,8 +1,8 @@
 const init = () => {
-  fetch('http://127.0.0.1:5001/wakeUp/')
+  fetch('http://127.0.0.1:5001/wakeUp')
     .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
+    .then(data => console.log('Backend Status:: ' + data.status))
+    .catch(error => console.log('Backend Status:: ' + error));
 };
 
 window.onload = init();
