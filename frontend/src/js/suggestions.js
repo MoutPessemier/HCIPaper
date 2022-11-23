1af3fc3e89714c34f0513c47c2f871e8c3fb39a1
Mout Pessemier <moutpessemier@hotmail.com>
2022-11-23 16:59:00 +0100

    Added dog modal + style changes

--- frontend/src/js/suggestions.js
+++ frontend/src/js/suggestions.js
@@ -1,3 +1,5 @@
+import * as bootstrap from 'bootstrap';
+
 const generateCards = (container, dog) => {
   const card = document.createElement('div');
   card.classList.add('card');
@@ -21,17 +23,86 @@ const generateCards = (container, dog) => {
 
   const cardText = document.createElement('p');
   cardText.classList.add('card-text');
-  cardText.innerHTML = dog.description;
+  cardText.innerHTML = dog.summary;
+
+  const cardFooter = document.createElement('div');
+  cardFooter.classList.add('card-footer');
+
+  const modalBtn = document.createElement('button');
+  modalBtn.classList.add('btn');
+  modalBtn.classList.add('btn-primary');
+  modalBtn.classList.add('btnm');
+  modalBtn.setAttribute('type', 'button');
+  modalBtn.setAttribute('data-bs-toggle', 'modal');
+  modalBtn.setAttribute('data-bs-target', `#dogModal${dog.name}`);
+  modalBtn.innerHTML = `Bekijk ${dog.name}`;
+
+  const modal = new bootstrap.Modal(document.getElementById(`dogModal${dog.name}`), { focus: true, keyboard: true });
+  modalBtn.addEventListener('click', e => {
+    modal.toggle();
+  });
+
+  imgContainer.appendChild(cardImg);
 
   cardBody.appendChild(cardTitle);
   cardBody.appendChild(cardText);
 
-  imgContainer.appendChild(cardImg);
+  cardFooter.appendChild(modalBtn);
 
   card.appendChild(imgContainer);
   card.appendChild(cardBody);
+  card.appendChild(cardFooter);
 
   container.appendChild(card);
+};
+
+const generateModal = (container, dog) => {
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.classList.add('fade');
+  modal.setAttribute('id', `dogModal${dog.name}`);
+  modal.setAttribute('tabindex', '-1');
+  modal.setAttribute('aria-labelledby', `dogModalLabel${dog.name}`);
+  modal.setAttribute('aria-hidden', 'true');
+
+  const modalDialog = document.createElement('div');
+  modalDialog.classList.add('modal-dialog');
+  modalDialog.classList.add('modal-dialog-scrollable');
+
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
+
+  const modalHeader = document.createElement('div');
+  modalHeader.classList.add('modal-header');
+
+  const title = document.createElement('h1');
+  title.classList.add('modal-title');
+  title.classList.add('fs-5');
+  title.setAttribute('id', `dogModalLabel${dog.name}`);
+  title.innerHTML = dog.name;
+
+  const closeBtn = document.createElement('button');
+  closeBtn.classList.add('btn-close');
+  closeBtn.setAttribute('type', 'button');
+  closeBtn.setAttribute('data-bs-dismiss', 'modal');
+  closeBtn.setAttribute('aria-label', 'Close');
+
+  modalHeader.appendChild(title);
+  modalHeader.appendChild(closeBtn);
+
+  const modalBody = document.createElement('div');
+  modalBody.classList.add('modal-body');
+  modalBody.setAttribute('id', `modalBody${dog.name}`);
+  modalBody.innerHTML = dog.description;
+
+  modalContent.appendChild(modalHeader);
+  modalContent.appendChild(modalBody);
+
+  modalDialog.appendChild(modalContent);
+
+  modal.appendChild(modalDialog);
+
+  container.appendChild(modal);
 };
 
 const getSliderValueForQuestion = name => {
@@ -47,6 +118,7 @@ const getSliderValueForQuestion = name => {
 
 const init = () => {
   const cardsContainer = document.getElementById('cards-container');
+  const modalContainer = document.getElementById('modal-container');
   fetch(
     'http://127.0.0.1:5001/getRecommendation?' +
       new URLSearchParams({
@@ -56,6 +128,7 @@ const init = () => {
     .then(res => res.json())
     .then(data => {
       data.recommendations.forEach(dog => {
+        generateModal(modalContainer, dog);
         generateCards(cardsContainer, dog);
       });
     })
