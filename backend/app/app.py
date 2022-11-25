1aec532f5983b45fa5e9b06ee5d8897d46fcd5ee
Mout Pessemier <moutpessemier@hotmail.com>
2022-11-25 15:37:05 +0100

    Dockerised backend

--- backend/app/app.py
+++ backend/app/app.py
@@ -113,6 +113,6 @@ def wakeup():
     return {'status': 'OK'}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5001", debug=True)
+    app.run(host="test.com", port="5000", debug=True)
 
 
