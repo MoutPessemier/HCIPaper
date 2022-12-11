2177bee52b5f20cad27fbd5c17bc43698ec01400
Mout Pessemier <moutpessemier@hotmail.com>
2022-12-11 12:33:38 +0100

    Finalisation of the frontend

--- frontend/src/js/landing.js
+++ frontend/src/js/landing.js
@@ -1,5 +1,5 @@
 const init = () => {
-  // fetch('http://127.0.0.1:5000/wakeUp')
+  //fetch('http://127.0.0.1:3490/fmmi9/wakeUp')
   fetch('https://augment.cs.kuleuven.be/fmmi9/wakeUp')
     .then(res => res.json())
     .then(data => console.log('Backend Status:: ' + data.status))
